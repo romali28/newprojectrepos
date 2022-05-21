94059c465832f593abb46e7b8aa13cb32d86fb7d
romali28 <62212110+romali28@users.noreply.github.com>
2022-05-21 12:39:03 +0530

    Add files via upload

---
+++ SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Media;
+using Android.OS;
+using Android.Util;
+using Android.Widget;
+using FFImageLoading.Forms.Platform;
+using Java.Lang;
+using static Android.Provider.MediaStore;
+
+namespace Vaamoz.Droid
+{
+    [Activity(Label = "Vaamoz", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: false);
+            //CachedImageRenderer.InitImageViewHandler();
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+            System.Threading.Thread.Sleep(500);
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
